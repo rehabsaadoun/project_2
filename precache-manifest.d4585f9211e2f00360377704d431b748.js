919b6868fa91b4acc3d2e924344095e2674bedd4
rehabsaadoun <YOUR GITHUB EMAIL>
2019-04-08 16:18:29 +0300

    Updates

---
+++ precache-manifest.d4585f9211e2f00360377704d431b748.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf045c0b5806f11623398c99ee0168b9",
+    "url": "/project_2/static/media/8.bf045c0b.jpg"
+  },
+  {
+    "revision": "3cd867730607e4afd6d28955d261ef51",
+    "url": "/project_2/static/media/5.3cd86773.jpg"
+  },
+  {
+    "revision": "c946fab5e257df87e582b7634dc81957",
+    "url": "/project_2/static/media/4.c946fab5.PNG"
+  },
+  {
+    "revision": "8caec97dc0f57022c788f91dbdbee655",
+    "url": "/project_2/static/media/18.8caec97d.PNG"
+  },
+  {
+    "revision": "a1b30321f5fb7a1d2f25",
+    "url": "/project_2/static/js/runtime~main.cf167350.js"
+  },
+  {
+    "revision": "8c90c0de05c0b7101083",
+    "url": "/project_2/static/js/main.f8ccfcfa.chunk.js"
+  },
+  {
+    "revision": "c9783493058e03c3d9ef",
+    "url": "/project_2/static/js/2.f57c90b3.chunk.js"
+  },
+  {
+    "revision": "8c90c0de05c0b7101083",
+    "url": "/project_2/static/css/main.d986e92c.chunk.css"
+  },
+  {
+    "revision": "9d11940e382e0a1455dd1bc56947c232",
+    "url": "/project_2/index.html"
+  }
+];
